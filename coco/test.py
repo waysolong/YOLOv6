43661b6eeec4f9660bc5e292805d2b9df3f42be2
wuxulong19950206 <1287173754@qq.com>
2022-09-16 20:37:19 +0800

    copy

---
+++ coco/test.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/8 14:32
+# @Author  : lazerliu
+# @File    : CocoForm_Learn.py
+import sys
+import json
+
+# sys.path.append("/xxx/cocoapi/PythonAPI")#把cocoapi的绝对路径加上
+from pycocotools.coco import COCO
+
+ann_file = "annotations/instances.json"# json文件的绝对路径
+coco = COCO(annotation_file=ann_file)
+
+print("coco\nimages.size [%05d]\tannotations.size [%05d]\t category.size [%05d]\ndone!"
+      %(len(coco.imgs),len(coco.anns),len(coco.cats)))
